3f3109887a5483766c542057e8957d84004fd99d
Carlos Cámara <carcam@users.noreply.github.com>
2025-01-28 22:38:47 +0100

    Fixed not being able to add tasks on empty list

--- src/component/admin/com_ctl/tmpl/tasks/default.php
+++ src/component/admin/com_ctl/tmpl/tasks/default.php
@@ -5,42 +5,42 @@ use Joomla\CMS\Router\Route;
 \defined('_JEXEC') or die;
 
 ?>
-<?php if ($this->items):?>
 <form action="<?php echo Route::_('index.php?option=com_ctl&view=tasks'); ?>" method="post" name="adminForm" id="adminForm">
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <caption>Tasks List</caption>
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Task</td>
-                    <td>Deadline</td>
-                    <td>Created</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($this->items as $task) :?>
-                <tr>
-                    <td><?php echo $task->id;?></td>
-                    <td>
-                        <div class="item-title">
-                            <a href="<?php echo Route::_('index.php?option=com_ctl&view=task&layout=edit&id=' . (int) $task->id); ?>">
-                                <?php echo $task->title;?>
-                            </a>
-                        </div>
-                        <p class="task-description"><?php echo $task->description;?></p>
-                    </td>
-                    <td><?php echo $task->deadline;?></td>
-                    <td><?php echo $task->created;?></td>
-                </tr>
-                <?php endforeach;?>
-            </tbody>
-        </table>
-    </div>
+    <?php if ($this->items): ?>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <caption>Tasks List</caption>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Task</td>
+                        <td>Deadline</td>
+                        <td>Created</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($this->items as $task) : ?>
+                        <tr>
+                            <td><?php echo $task->id; ?></td>
+                            <td>
+                                <div class="item-title">
+                                    <a href="<?php echo Route::_('index.php?option=com_ctl&view=task&layout=edit&id=' . (int) $task->id); ?>">
+                                        <?php echo $task->title; ?>
+                                    </a>
+                                </div>
+                                <p class="task-description"><?php echo $task->description; ?></p>
+                            </td>
+                            <td><?php echo $task->deadline; ?></td>
+                            <td><?php echo $task->created; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    <?php else : ?>
+        <div class="text-large">
+            You cleared your inbox. Yeah!!!
+        </div>
+    <?php endif; ?>
     <input type="hidden" name="task" value="">
 </form>
-<?php else : ?>
-<div class="text-large">
-    You cleared your inbox. Yeah!!!
-</div>
-<?php endif;?>
